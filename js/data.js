f2cbeda45f70599c6dd3e2f0473c983b7a6f1824
Reem Saijary <reemsaijary31@gmail.com>
2026-07-18 02:57:08 +0300

    Configure global styles and reusable layout foundation

---
+++ js/data.js
@@ -0,0 +1,7 @@
+/*
+    Website Data
+    Purpose:
+    - Store dynamic website content in a structured format.
+    - Includes projects, skills, experience, education,
+    certificates, and other reusable data.
+*/
--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+/*
+    Main JavaScript
+    Purpose:
+    - Controls website functionality and interactions.
+    - Handles dynamic rendering, navigation behavior,
+    animations, and other client-side logic.
+*/
+
 console.log("Portfolio website JavaScript connected successfully.");
